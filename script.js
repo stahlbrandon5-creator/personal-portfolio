c388815e9cd7e3bc95eb9096cf7c9e54734a2900
stahl <stahl.brandon4@gmail.com>
2026-05-15 01:51:42 -0400

    added toggle js for dropdown lists

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-document.querySelector('.show-more')
-document.querySelectorAll('show-more')
-
-GamepadButton.addEventListener('click', function() {
-
-});
-
 const buttons = document.querySelectorAll('.show-more');
 
 buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        console.log('Button clicked!');
+    button.addEventListener('click', () => {
+        const contentList  = button.closest('.content-list');
+        const toggleItems = contentList.querySelectorAll(".toggle");
+        
+        let isToggled = false;
+
+        toggleItems.forEach(function(item) {   
+            isToggled = item.classList.toggle("hidden");
+            
+        });
+
+        button.textContent = isToggled ? 'Show more ▼' : 'Show less ▲' ;
     });
 });
